66c96b74dd6124747aca92784c608213fd0797e0
Karla Sanchez <ksoalianza@gmail.com>
2024-02-08 19:36:09 -0600

    strStr, turns out the first approach with find() is O(n) and the second one using substr is O(nxn) because the method is O(n) and then the for loop

--- strStr.cpp
+++ strStr.cpp
@@ -3,13 +3,33 @@
 
 using namespace std;
 
-int strStr(string haystack, string needle){
+int strStr(string haystack, string needle)
+{
     size_t found = haystack.find(needle);
     return found;
 }
-int main(){
 
-    cout << strStr("leetcode", "leet") << endl;
-    
+int strStrHardCore(string haystack, string needle)
+{   
+    if(needle.size() > haystack.size()){
+        return -1;
+    }
+    // cout <<  haystack.size() - needle.size()  << endl;
+    for (int i = 0; i <= haystack.size() - needle.size(); i++)
+    {
+        string window = haystack.substr(i,needle.size());
+        if (window == needle)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+
+    // cout << strStr("leetcode", "leet") << endl;
+    // cout << strStrHardCore("leetcode", "leet") << endl;
+    cout << strStrHardCore("codeleet", "et") << endl;
     return 0;
 }
